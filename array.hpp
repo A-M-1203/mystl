c909aaf67b2c156814ac622865e720eeb27211f7
Aleksa <aleksa.milosevic807@gmail.com>
2026-03-02 23:57:39 +0100

    Add basic methods for array data structure

--- array.hpp
+++ array.hpp
@@ -1,15 +1,40 @@
 #include <cstdint>
+#include <stdexcept>
 
 namespace mystl {
-template <typename Type, uint64_t Capacity>
-class Array {
+template <typename T, uint64_t Size>
+class array {
 public:
-    constexpr uint64_t capacity() { return Capacity; }
-    Type &operator[](uint64_t index) { return data[index]; }
-    const Type &operator[](uint64_t index) const { return data[index]; }
+    constexpr T& at(uint64_t index) {
+        if (index >= Size)
+            throw std::out_of_range("Index out of range");
+        return _elements[index];
+    }
+
+    constexpr const T& at(uint64_t index) const {
+        if (index >= Size)
+            throw std::out_of_range("Index out of range");
+        return _elements[index];
+    }
+
+    constexpr T& front() { return _elements[0]; }
+    constexpr const T& front() const { return _elements[0]; }
+
+    constexpr T& back() { return _elements[Size - 1]; }
+    constexpr const T& back() const { return _elements[Size - 1]; }
+
+    constexpr T* data() noexcept { return _elements; }
+    constexpr const T* data() const noexcept { return _elements; }
+
+    constexpr bool empty() const noexcept { return Size == 0; }
+
+    constexpr uint64_t size() const noexcept { return Size; }
+    constexpr T& operator[](uint64_t index) { return _elements[index]; }
+    constexpr const T& operator[](uint64_t index) const {
+        return _elements[index];
+    }
 
 private:
-    uint64_t size = 0;
-    Type data[Capacity];
+    T _elements[Size];
 };
 } // namespace mystl
--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
 #include "array.hpp"
-
+#include <array>
+#include <iostream>
 int main() {
-    mystl::Array<int, 5> array;
+    mystl::array<int, 5> array;
     array[0] = 1;
     array[1] = 2;
     array[2] = 3;
     array[3] = 4;
     array[4] = 5;
-    for(uint64_t i = 0; i < array.capacity(); i++){
+    for (uint64_t i = 0; i < array.size(); i++) {
         std::cout << array[i] << std::endl;
     }
+    std::array<int, 3> arr;
 }
