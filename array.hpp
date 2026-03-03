b70ba5406fd6ccead0853e5a5618f2e84f550bdd
Aleksa <aleksa.milosevic807@gmail.com>
2026-03-04 00:59:22 +0100

    Add iterators for array data structure

--- array.hpp
+++ array.hpp
@@ -1,53 +1,67 @@
-#include <cstdint>
 #include <stdexcept>
 
 namespace mystl {
-template <typename T, uint64_t Size>
+template <typename T, size_t Size>
 struct array {
-    constexpr T& at(uint64_t index) {
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef value_type* iterator;
+    typedef const value_type* const_iterator;
+    typedef size_t size_type;
+
+    constexpr reference at(size_type index) {
         if (index >= Size)
             throw std::out_of_range("Index out of range");
         return _elements[index];
     }
-    constexpr const T& at(uint64_t index) const {
+    constexpr const_reference at(size_type index) const {
         if (index >= Size)
             throw std::out_of_range("Index out of range");
         return _elements[index];
     }
 
-    constexpr T& operator[](uint64_t index) { return _elements[index]; }
-    constexpr const T& operator[](uint64_t index) const {
-        return _elements[index];
-    }
+    constexpr reference operator[](size_type index) { return _elements[index]; }
+    constexpr const_reference operator[](size_type index) const { return _elements[index]; }
 
-    constexpr T& front() { return _elements[0]; }
-    constexpr const T& front() const { return _elements[0]; }
+    constexpr reference front() { return _elements[0]; }
+    constexpr const_reference front() const { return _elements[0]; }
 
-    constexpr T& back() { return _elements[Size - 1]; }
-    constexpr const T& back() const { return _elements[Size - 1]; }
+    constexpr reference back() { return _elements[Size - 1]; }
+    constexpr const_reference back() const { return _elements[Size - 1]; }
 
-    constexpr T* data() noexcept { return _elements; }
-    constexpr const T* data() const noexcept { return _elements; }
+    constexpr pointer data() noexcept { return _elements; }
+    constexpr const pointer data() const noexcept { return _elements; }
+
+    constexpr iterator begin() noexcept { return iterator(data()); }
+    constexpr const_iterator begin() const noexcept { return const_iterator(data()); }
+    constexpr const_iterator cbegin() const noexcept { return const_iterator(data()); }
+
+    constexpr iterator end() noexcept { return iterator(data() + Size); }
+    constexpr const_iterator end() const noexcept { return const_iterator(data() + Size); }
+    constexpr const_iterator cend() const noexcept { return const_iterator(data() + Size); }
 
     constexpr bool empty() const noexcept { return Size == 0; }
 
-    constexpr uint64_t size() const noexcept { return Size; }
+    constexpr size_type size() const noexcept { return Size; }
 
-    constexpr void fill(const T& value) {
-        for (uint64_t i = 0; i < Size; i++) {
+    constexpr void fill(const_reference value) {
+        for (size_type i = 0; i < Size; i++) {
             _elements[i] = value;
         }
     }
 
     constexpr void swap(array& other) noexcept {
-        T temp[Size];
-        for (uint64_t i = 0; i < Size; i++) {
+        value_type temp[Size];
+        for (size_type i = 0; i < Size; i++) {
             temp[i] = _elements[i];
             _elements[i] = other[i];
             other[i] = temp[i];
         }
     }
 
-    T _elements[Size];
+    value_type _elements[Size];
 };
 } // namespace mystl
--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 #include "array.hpp"
+#include <array>
 #include <iostream>
 
 int main() {
     mystl::array<int, 5> array1 = {1, 2, 3, 4, 5};
     mystl::array<int, 5> array2{6, 7, 8, 9, 10};
+    std::array<int, 5> arr;
 
     std::cout << "Array1:\n";
-    for (uint64_t i = 0; i < array1.size(); i++) {
+    for (size_t i = 0; i < array1.size(); i++) {
         std::cout << array1[i] << std::endl;
     }
     std::cout << "Array2:\n";
-    for (uint64_t i = 0; i < array2.size(); i++) {
+    for (size_t i = 0; i < array2.size(); i++) {
         std::cout << array2[i] << std::endl;
     }
 
     array1.swap(array2);
 
     std::cout << "Array1:\n";
-    for (uint64_t i = 0; i < array1.size(); i++) {
-        std::cout << array1[i] << std::endl;
+    for (auto& value : array1) {
+        std::cout << value << std::endl;
     }
     std::cout << "Array2:\n";
-    for (uint64_t i = 0; i < array2.size(); i++) {
-        std::cout << array2[i] << std::endl;
+    for (auto it = array2.begin(); it != array2.end(); it++) {
+        std::cout << *it << std::endl;
     }
 }
